041d26064553c25d7df15429c36dd56a9fd42b6b
aoa9 <aoa9@cornell.edu>
2022-04-03 23:19:33 -0400

    fix authorization

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const https = require("https")
+const client = require("@mailchimp/mailchimp_marketing");
+require('dotenv').config()
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+
+const apikey = process.env.API_KEY
+const listid = process.env.LIST_ID
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html")
@@ -14,7 +19,14 @@ app.post("/", function(req, res){
     const firstname  =  req.body.firstname
     const lastname = req.body.lastname
     const emailAddress = req.body.email
-    const data = {
+
+    client.setConfig({
+    apiKey: apikey,
+    server: "us20",
+    });
+
+    const run = async () => {
+    const response = await client.lists.batchListMembers(listid, {
         members: [
             {
                 email_address: emailAddress,
@@ -24,26 +36,18 @@ app.post("/", function(req, res){
                     LNAME: lastname
                 }
             }
-        ]
+        ],
+    });
+    //This is a temporary fix. Response was a mess to read. 
+    if (response != null){
+        res.sendFile(__dirname + "/success.html")
+    } else {
+        res.sendFile(__dirname + "/failure.html")
     }
-    const jsonData = JSON.stringify(data);
+    };
 
-    const url = "https://us20.api.mailchimp.com/3.0/lists/LISTID/"
-    const options = {
-        method: "POST",
-        auth: "API KEY"
-    }
-    const request = https.request(url, options, function(response){
-        response.on('data', function(data){
-        })
-        if (response.statusCode === 200){
-            res.sendFile(__dirname + "/success.html")
-        } else {
-            res.sendFile(__dirname + "/failure.html")
-        }
-    })
-    request.write(jsonData);
-    request.end();
+    run();
+
 });
 
 app.post('/failure', function(req, res){
